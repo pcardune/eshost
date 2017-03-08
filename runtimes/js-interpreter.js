caa15e70669750cbebb2c89d7492ed5dd42c15ee
Paul Carduner <paul@carduner.net>
2017-03-08 19:02:44 +0000

    Add createRealm and evalScript methods

--- runtimes/js-interpreter.js
+++ runtimes/js-interpreter.js
@@ -1,10 +1,40 @@
 var $ = {
   global: this,
   createRealm: function (options) {
-    throw new Error("createRealm not implemented yet...");
+    options = options || {};
+    options.globals = options.globals || {};
+
+    context = {
+      console: console,
+      vm: vm,
+    };
+    for (var glob in options.globals) {
+      context[glob] = options.globals[glob];
+    }
+
+    vm.runInContext(this.source, context);
+
+    context.$.source = this.source;
+    context.$.context = context;
+    context.$.destroy = function () {
+      if (options.destroy) {
+        options.destroy();
+      }
+    };
+    return context.$;
   },
   evalScript: function (code) {
-    throw new Error("evalScript not implemented yet...");
+    try {
+      if (this.context) {
+        vm.runInContext(code, this.context);
+      } else {
+        eval(code);
+      }
+
+      return { type: 'normal', value: undefined };
+    } catch (e) {
+      return { type: 'throw', value: e };
+    }
   },
   getGlobal: function (name) {
     return this.global[name];
